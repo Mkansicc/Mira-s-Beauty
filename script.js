259e0373e3fcf5f64f38bcf503fee6e6714bd7b2
Mkansicc <108984577+Mkansicc@users.noreply.github.com>
2026-03-06 21:29:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener("click", function (e) {
     const targetId = this.getAttribute("href");
@@ -5,9 +12,8 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
 
     if (target) {
       e.preventDefault();
-      target.scrollIntoView({
-        behavior: "smooth"
-      });
+      target.scrollIntoView({ behavior: "smooth" });
+      navLinks.classList.remove("show");
     }
   });
 });
